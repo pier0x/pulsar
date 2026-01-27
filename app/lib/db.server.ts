3770408b0bb79884a0cdf1acdaefc4c9cea01d59
Pier <36680039+pier0x@users.noreply.github.com>
2026-01-27 22:37:48 +0000

    fix: remove SQLite adapter, use standard Prisma client for PostgreSQL

--- app/lib/db.server.ts
+++ app/lib/db.server.ts
@@ -1,6 +1,4 @@
 import { PrismaClient } from "../../generated/prisma/client";
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
-import { config } from "./config.server";
 
 let prisma: PrismaClient;
 
@@ -10,16 +8,13 @@ declare global {
 }
 
 const createPrismaClient = () => {
-  const adapter = new PrismaBetterSqlite3({
-    url: config("database.connections.sqlite.url"),
-  });
-  return new PrismaClient({ adapter });
+  return new PrismaClient();
 };
 
 // This is needed because in development we don't want to restart
 // the server with every change, but we want to make sure we don't
 // create a new connection to the DB with every change either.
-if (config("app.env") === "production") {
+if (process.env.NODE_ENV === "production") {
   prisma = createPrismaClient();
 } else {
   if (!global.__db__) {
